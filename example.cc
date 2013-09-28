97a28e1cad26f1c95f5293c3a9bf50467efff664
Zhihao Yuan <lichray@gmail.com>
2013-09-27 23:26:12 -0400

    Fancier example.

--- example.cc
+++ example.cc
@@ -6,7 +6,7 @@ using stdex::randreal;
 
 int main() {
 	std::cout << randint(0, 6) << std::endl;
-	std::cout << randint(0, 6) << std::endl;
+	std::cout << randint('A', 'G') << std::endl;
 	std::cout << randreal(0.0, 6.0) << std::endl;
-	std::cout << randreal(0.0, 6.0) << std::endl;
+	std::cout << randreal(0.0f, 6.0f) << std::endl;
 }
